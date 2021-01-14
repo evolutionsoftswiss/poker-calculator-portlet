d1529a77594391dda151748d1a960be756afab51
evolutionsoft <30459276+evolutionsoftswiss@users.noreply.github.com>
2021-01-14 19:35:06 +0100

    Initial share develop commit

---
+++ poker-simulator/src/main/java/ch/evolutionsoft/poker/calculator/util/PokersourceLibraryLoader.java
@@ -0,0 +1,30 @@
+package ch.evolutionsoft.poker.calculator.util;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.tomcat.jni.Library;
+
+public class PokersourceLibraryLoader {
+
+	static boolean initialized = true;
+
+	public synchronized static void init() {
+
+		if (!initialized) {
+
+			Properties liferayPluginPackageProperties = new Properties();
+			try {
+				liferayPluginPackageProperties.load(PokersourceLibraryLoader.class.getResourceAsStream("/library.properties"));
+			} catch (IOException ioe) {
+				ioe.printStackTrace();
+			}
+			String libraryBasePath = liferayPluginPackageProperties.getProperty("libraryPath");
+
+			Library.load(libraryBasePath + "libpoker-eval.so");
+			Library.load(libraryBasePath + "libpokerjni.so");
+
+			initialized = true;
+		}
+	}
+}
